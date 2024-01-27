f53cf38163647b70d59d25fc2dc9004f1d735527
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2024-01-27 11:06:02 -0800

    ESLint format

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,20 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-    entry: ["./src/index.js"],
-    output: {
-        filename: "main.js",
-        path: path.resolve(__dirname, "dist"),
-        assetModuleFilename: "imgs/[name].[ext]", 
-        assetModuleFilename: "svgs/[name].[ext]",
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(png|jpe?g|gif|svg)$/i,
-                type: "asset/resource",
-            },
-        ],
-    },
+  entry: ['./src/index.js'],
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, "dist"),
+    assetModuleFilename: 'imgs/[name].[ext]',
+    // eslint-disable-next-line no-dupe-keys
+    assetModuleFilename: 'svgs/[name].[ext]',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
 };
