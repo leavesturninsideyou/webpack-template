904750e92e9d5edb9b02931a7d4219827aad59d9
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2024-01-27 11:16:47 -0800

    Modify README

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,21 +3,24 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: "airbnb-base",
+  extends: [
+    'airbnb-base',
+    'prettier',
+  ],
   overrides: [
     {
       env: {
         node: true,
       },
-      files: [".eslintrc.{js,cjs}"],
+      files: ['.eslintrc.{js,cjs}'],
       parserOptions: {
-        sourceType: "script",
+        sourceType: 'script',
       },
     },
   ],
   parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
   rules: {},
 };
--- README.md
+++ README.md
@@ -1,6 +1,3 @@
 After cloning this repo, do the following:
 
-1. npm install --save-dev webpack
-2. npm init @eslint/config
-3. npm install --save-dev --save-exact prettier
-4. npm install --save-dev eslint-config-prettier
+`npm i`
