51c6d077b0cafba6a0c0553a54b9daecd0b2afd9
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2024-02-19 11:34:06 -0800

    Add double quote format
    Single quotes are an inconvenience to get used to.

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,23 +4,28 @@ module.exports = {
     es2021: true,
   },
   extends: [
-    'airbnb-base',
-    'prettier',
+    "airbnb-base",
+    "prettier",
   ],
   overrides: [
     {
       env: {
         node: true,
       },
-      files: ['.eslintrc.{js,cjs}'],
+      files: [".eslintrc.{js,cjs}"],
       parserOptions: {
-        sourceType: 'script',
+        sourceType: "script",
       },
     },
   ],
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
-  rules: {},
+  rules: {
+    "quotes": [
+      "error",
+      "double"
+    ]
+  },
 };
